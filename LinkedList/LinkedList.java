2c37e8565431796e3daa966491d57259eb43a718
Isha Singh <study.luto@gmail.com>
2026-03-17 23:48:13 +0530

    remove

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -70,7 +70,26 @@ public class LinkedList {
         return val;
     }
 
-    public int 
+    public int removeLast (){
+        if(size==0){
+            System.out.println("ll is empty");
+            return Integer.MIN_VALUE;
+        }else if(size==1){
+            int val = head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        Node prev = head;
+        for (int i = 0; i < size-2; i++) {
+            prev = prev.next;
+        }
+        int val = prev.next.data;
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+    }
 
     public void print(){
         Node temp = head;
@@ -101,6 +120,9 @@ public class LinkedList {
         System.out.println(ll.size);
         ll.removeFirst();
         ll.print();
+        ll.removeLast();
+        ll.print();
+        System.out.println(ll.size);
     }
 
 }
