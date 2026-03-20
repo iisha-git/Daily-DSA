24be4492a24cd1de0e5dd88fbdd6eeef59a784bc
Isha Singh <study.luto@gmail.com>
2026-03-20 15:21:08 +0530

    palindrome , nth node remove from last , reverse

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -91,6 +91,18 @@ public class LinkedList {
         return val;
     }
 
+    public int itrSearch(int key){
+        Node temp = head;
+        int i=0;
+        while(temp!=null){
+            if(temp.data == key){
+                return i;
+            }
+            temp = temp.next;
+            i++;
+        }
+        return -1;
+    }
     public void print(){
         Node temp = head;
         if(head==null){
@@ -104,6 +116,111 @@ public class LinkedList {
         System.out.println("null");
     }
 
+    public int helper(Node head, int key){
+        if(head==null){
+            return -1;
+        }
+
+        if(head.data == key){
+            return 0;
+        }
+
+        int idx = helper(head.next, key);
+        if(idx==-1){
+            return -1;
+        }
+        return idx+1;
+    }
+
+    public int recSearch(int key){
+        return helper(head, key);
+    }
+
+
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public void deleteNthfromEnd(int n){
+        int sz = 0;
+        Node temp = head; 
+        while(temp!=null){
+            temp = temp.next;
+            sz++;
+        }
+
+        if(n==sz){//remove first
+            head = head.next;
+            sz--;
+            return;
+        }
+
+        int i = 1;
+        int iToFind = sz-n;
+        Node prev = head;
+        while(i<iToFind){
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+
+    //slow fast approach
+    public Node findMid(Node head){ // helper for  palindrome
+        Node slow = head;
+        Node fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean checkPalindrome(){
+        if(head==null || head.next==null){
+            return true;
+        }
+        //step 1 - find mid -slow fast approach
+        Node midNode = findMid(head);
+
+        //step 2 - reverse 2nd half
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while(curr!=null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node right = prev; // head of right half
+        Node left = head;
+
+        //step 3 - check if right half is same as left half
+        while(right!=null){
+            if(left.data!=right.data){
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+
+    /*
     public static void main(String[]args){
         LinkedList ll = new LinkedList();
         ll.print();
@@ -123,6 +240,24 @@ public class LinkedList {
         ll.removeLast();
         ll.print();
         System.out.println(ll.size);
+        System.out.println(ll.itrSearch(3));     
+    }
+    */
+
+        public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.add(2, 3);
+
+        ll.print(); //1->2->3->4->5
+        ll.reverse();
+        ll.print();
+        ll.deleteNthfromEnd(3);
+        ll.print();
+        System.out.println(ll.checkPalindrome());
     }
 
 }
