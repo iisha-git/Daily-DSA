953354e50565d1e761184b4451b43443f7d93b7f
Isha Singh <study.luto@gmail.com>
2026-04-04 23:47:42 +0530

    deque

---
+++ Queue/QueueDeque.java
@@ -0,0 +1,28 @@
+package Queue;
+import java.util.*;
+
+public class QueueDeque {
+    static class Queue{
+        Deque<Integer> deque = new LinkedList<>();
+        public void add(int data){
+            deque.addLast(data);
+        }
+        public int remove(){
+            return deque.removeFirst();
+        }
+        public int peek(){
+            return deque.getFirst();
+        }
+    }
+    public static void main(String[] args) {
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println("peek="+q.peek());
+        q.remove();
+        q.remove();
+        q.remove();
+    }
+}
