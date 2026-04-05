144e1d36cc2f1f1dde9a8de2dc7dd85da368c971
Isha Singh <study.luto@gmail.com>
2026-04-05 21:33:37 +0530

    activity elector logic

---
+++ Greedy Algorithms/ActivitySelector.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ActivitySelector {
+    public static void main(String[] args) {
+        int start[] = {1,3 ,0,5,8,5};
+        int end[] = {2,4,6,7,9,9};
+
+        int maxAct = 0;
+        ArrayList<Integer> ans = new ArrayList<>();
+        //1st activity
+        maxAct = 1;
+        ans.add(0);
+
+        int lastEnd = end[0];
+        
+        for (int i = 0; i < end.length; i++) {
+            
+        }
+
+    }
+}
+    
