a7f42e9bcf97a7e7a00a268c8b8c36c707ea98e9
Isha Singh <study.luto@gmail.com>
2026-04-07 23:17:18 +0530

    knapsnack

--- Greedy Algorithms/ActivitySelector.java
+++ Greedy Algorithms/ActivitySelector.java
@@ -5,21 +5,39 @@ public class ActivitySelector {
         int start[] = {1,3 ,0,5,8,5};
         int end[] = {2,4,6,7,9,9};
 
+        //sorting of 2d array
+        int activities[][] = new int[start.length][3];
+        for (int i = 0; i < start.length; i++) {
+            activities[i][0]=i;
+            activities[i][1]=start[i];
+            activities[i][2]=end[i];
+        }
+
+        //lambda function->shortform
+        Arrays.sort(activities,Comparator.comparingDouble(o->o[2])); //sorting is based on coloumn 2
+
+
+
         int maxAct = 0;
         ArrayList<Integer> ans = new ArrayList<>();
         //1st activity
-        maxAct = 1;
-        ans.add(0);
-
-        int lastEnd = end[0];
+        maxAct = 1; 
+        ans.add(activities[0][0]);
+        int lastEnd =activities[0][2];
         
         for (int i = 0; i < end.length; i++) {
-            if(start[i]>=lastEnd){
+            if(activities[i][1]>=lastEnd){
                 maxAct++;
-                ans.add(i);
-                
+                ans.add(activities[i][0]);
+                lastEnd=activities[i][2];
             }
         }
+
+        System.out.println("max axtivities = " +maxAct);
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print("A"+ans.get(i)+" ");
+        }
+        System.out.println();
 
     }
 }
