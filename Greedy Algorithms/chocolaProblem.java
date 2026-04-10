d7a12a783270383bae7ebb6e5a47c5ea3c5b3c30
Isha Singh <study.luto@gmail.com>
2026-04-10 22:02:29 +0530

    greedy continued

---
+++ Greedy Algorithms/chocolaProblem.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class chocolaProblem{
+    public static void main(String[] args) {
+        int horLen = 4 , verLen = 6;
+        Integer costVer[] = {2,1,3,1,4};
+        Integer costHor[] = {4,1,2}; 
+
+        Arrays.sort(costVer,Collections.reverseOrder());
+        Arrays.sort(costHor,Collections.reverseOrder());
+
+        int h = 0, v =0;
+        int hp = 1 , vp=1n ;
+        int cost = 0;
+
+        while (h<horLen && v<verLen) {
+            if(costVer[v]<=costHor[h]){
+                cost+=(costHor[h]*vp);
+                h++;
+                hp++;
+            }else{
+                cost+=(costVer[v]*hp);
+                vp++;
+                v++;
+            }
+        }
+
+        while(h<costHor.length){
+            cost+=(costHor[h]*vp);
+            hp++;
+            h++;
+        }
+        while(v<costHor.length){
+            cost+=(costHor[v]*hp);
+            vp++;
+            v++;
+        }
+        System.out.println("min cost of cuts = "+ cost);
+    }
+}
